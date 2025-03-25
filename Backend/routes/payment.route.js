332e2526ad12496f8038f071b3350a6919a10a20
bpdajang <burypaulo360@gmail.com>
2025-03-25 23:31:08 +0000

    Update payment.route.js

--- Backend/routes/payment.route.js
+++ Backend/routes/payment.route.js
@@ -1,13 +1,24 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import {
-  checkoutSuccess,
-  createCheckoutSession,
+  checkoutMomo,
+  cardPay,
+  paystackWebhook,
+  completeAuth,
 } from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
-router.post("/create-checkout-session", protectRoute, createCheckoutSession);
-router.post("/checkout-success", protectRoute, checkoutSuccess);
+router.post("/momo", protectRoute, checkoutMomo);
+router.post("/card", protectRoute, cardPay);
+router.post("/complete-auth", protectRoute, completeAuth);
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  paystackWebhook
+);
+
+
 
 export default router;
+
