dc2b90af388316bcc74ef85189d906c8270154b0
bpdajang <burypaulo360@gmail.com>
2025-03-25 01:00:26 +0000

    Delete Backend directory

--- Backend/Lib/redis.js
+++
@@ -1,8 +0,0 @@
-import Redis from "ioredis";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const redis = new Redis(process.env.Upstash_redis_uri);
-
-export default redis;
--- Backend/Lib/stripe.js
+++
@@ -1,3 +0,0 @@
-import stripe from "stripe";
-
-export const Stripe = new stripe(process.env.STRIPE_SECRET);
